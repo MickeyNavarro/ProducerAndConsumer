98be14ce91aacca7b3ad37f3c4d8ecc55b465c4f
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-10-14 21:19:46 -0700

    recommit the correct files

--- ProducerConsumer.c
+++ ProducerConsumer.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>  //Header file for sleep(). man 3 sleep for details.
 #include <pthread.h>
+#include <errno.h>
 
 //Almicke Navarro
 //CST-221
@@ -14,11 +15,19 @@
 //           https://pubs.opengroup.org/onlinepubs/9699919799/functions/pthread_join.html
 //           https://stackoverflow.com/questions/31082559/warning-passing-argument-1-of-pthread-join-makes-integer-from-pointer-without
 //           https://timmurphy.org/2010/05/04/pthreads-in-c-a-minimal-working-example/
+//           https://fresh2refresh.com/c-programming/c-programs/c-program-to-find-array-size/
+
 
 
 //define buffer
-#define BUFFER 10
-int buffer[BUFFER];
+#define MAX 10
+int buffer[MAX];
+
+//define a counter to determine how many products are produced/yet to be consumed
+int counter = 0;
+
+//define the size of buffer
+int length = sizeof(buffer)/sizeof(int);
 
 //define methods
 int produce();
@@ -31,30 +40,76 @@ int get();
 //code given by instructor
 int theProduct;
 
+// The Child PID if the Parent else the Parent PID if the Child
+pid_t otherPid;
+
+//method to simply increment the product, hence creating a new product
 int produce(){
     return theProduct+1;
 }
 
+//method to output that the product has been consumed
 void consume(int i){
     printf("Consumed: %i\n", i);
 }
 
+//method to put the product in the buffer
 void *producer() {
+    printf("/***Producing***/\n");
+    
     int i;
+    //loop for forever
     while(1){
-        i = produce();
-        put(i);
-        //allow the program to sleep for 1 sec to avoid a fast output
-        sleep(1);
+        //check if the buffer is full
+        if(length == counter){
+            
+            //if yes,
+            //output
+            printf("Buffer is full. No new products may be added.\n");
+            //put the producer to sleep
+            printf("Producer will now been put to sleep.\n");
+            sleep(60); //program will temperarily stop here
+        }
+        else {
+            //if no,
+            //output
+            printf("Buffer is not full. You may add new products.\n");
+            //produce the new product
+            i = produce();
+            //put the product into the buffer
+            put(i);
+            //increase counter
+            counter++;
+        }
+        
     }
 }
 
+//method to consumer the product from the buffer
 void *consumer() {
+    printf("/***Consuming***/\n");
+    
     int i;
+    //loop for forever
     while(1){
-        i = get();
-        consume(i);
-        sleep(1);
+        //check if buffer is full
+        if (counter <= 10 && counter > 0){
+            //if yes,
+            printf("Buffer is full. Products may now be consumed.\n");
+            //get the most recent product from the buffer
+            i = get();
+            //consume the product
+            consume(i);
+            //decrease the counter
+            counter--;
+            //check if the buffer is empty
+            if (length ==0){
+                printf("Buffer is empty. Products may NOT be consumed.\n");
+                printf("Consumer will now been put to sleep.\n");
+                //if yes, go to sleep
+                sleep(60);
+            }
+        }
     }
 }
 
@@ -65,14 +120,18 @@ void *consumer() {
 void put(int i) {
     //set the product to the given int
     theProduct = i;
+    //output what has been produced
     printf("Produced: %i\n", i);
+    //put into buffer
+    buffer[counter] = i;
     return;
 }
 
-//method to return the product
-//similar to produce() method, however it only returns the product it gets; does not increase the product
+//method to get the product from the buffer
 int get() {
-    return theProduct;
+    //get the most recent product produced from the buffer
+    int productToBeConsumed = buffer[counter];
+    return productToBeConsumed;
 }
 
 //main method will be used to manage the processes and threads
